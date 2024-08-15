66db5e3c4256aeafff74965138da7ef98bff55b0
jasper <whiteana001@warren.k12.in.us>
2024-08-15 12:40:09 -0400

    initial commit

--- src/posNegZero.java
+++ src/posNegZero.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class posNegZero {
+public class posNegZero {
+    public static void main(String[] args) {
+        checkNumber(0);
+    }
+    public static void checkNumber(int number){
+        if (number > 0){
+            System.out.println("positive");
+        } else if (number < 0){
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+
+    }
+
 }
